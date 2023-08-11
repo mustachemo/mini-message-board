42e867e02f6da4f4ceac963bb7d1418335d6d407
mustachemo <mhasan1080@gmail.com>
2023-08-11 00:57:22 -0700

    added sass middleware

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import nunjucks from 'nunjucks';
 import dotenv from 'dotenv';
 import logger from 'morgan';
 import cors from 'cors';
+import path from 'path';
+import sassMiddleware from 'node-sass-middleware';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-nunjucks.configure('src/views', {
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
 });
@@ -18,9 +20,17 @@ app.set('env', process.env.NODE_ENV);
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(cors());
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, 'styles'),
+    dest: path.join(__dirname, 'public/css'),
+    debug: true,
+    outputStyle: 'compressed',
+  })
+);
 
 app.use('/', indexRouter);
 app.use('/new', newMessageRouter);
