eadb9c75011111a7274eae525345d1ed05c37ef9
mustachemo <mhasan1080@gmail.com>
2023-08-11 01:13:13 -0700

    sass middleware not working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import nunjucks from 'nunjucks';
 import dotenv from 'dotenv';
 import logger from 'morgan';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
 import path from 'path';
 import sassMiddleware from 'node-sass-middleware';
 
@@ -18,9 +19,11 @@ nunjucks.configure('views', {
 app.set('view engine', 'njk');
 app.set('env', process.env.NODE_ENV);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(cors());
 app.use(
@@ -29,11 +32,13 @@ app.use(
     dest: path.join(__dirname, 'public/css'),
     debug: true,
     outputStyle: 'compressed',
+    error: err => console.log(err),
   })
 );
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/new', newMessageRouter);
+// app.use('/', indexRouter);
+// app.use('/new', newMessageRouter);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}!`);
