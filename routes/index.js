d09f957c06e548689669c46e1ed9a60bb17788bb
mustachemo <mhasan1080@gmail.com>
2023-08-11 11:45:15 -0700

    new njk works

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const indexRouter = express.Router();
 
 indexRouter.get('/', (req, res) => {
-  res.render('base');
+  res.render('index');
 });
 
 export default indexRouter;
