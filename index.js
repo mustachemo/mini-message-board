3004fd9256f15e8d6e686f888618a592ef986d42
mustachemo <mhasan1080@gmail.com>
2023-08-11 10:44:45 -0700

    serving css files, worked!

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 // app.use('/new', newMessageRouter);
